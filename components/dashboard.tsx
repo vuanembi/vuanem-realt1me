a85234ee4cd276bfaff21e9a080028e646e96a6c
HM <hieumdd@gmail.com>
2021-10-17 16:49:02 +0700

    feat: Added PollingInterval & Last Updated At

--- components/dashboard.tsx
+++ components/dashboard.tsx
@@ -1,47 +1,66 @@
 import React from 'react';
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+import { capitalize } from 'lodash';
 import useSWR from 'swr';
 import axios from 'axios';
-import { capitalize } from 'lodash';
-import { VStack, Text, Wrap, WrapItem, Heading, Spinner } from '@chakra-ui/react';
-
+import {
+  VStack,
+  Flex,
+  Text,
+  Wrap,
+  WrapItem,
+  Heading,
+  Spinner,
+} from '@chakra-ui/react';
 import Metric from './metric';
 
 import type { APIData, MetricData, DisplayProps } from '../types/data';
 
+dayjs.extend(relativeTime);
+
 const Dashboards = () => {
-  const { data, error } = useSWR<APIData, Error>('/api/salesOrder', (url) =>
-    axios.get(url).then((res) => res.data)
+  const { data, error } = useSWR<APIData, Error>(
+    '/api/salesOrder',
+    (url) => axios.get(url).then((res) => res.data),
+    { refreshInterval: 10000, refreshWhenHidden: true }
   );
-  if (!error && !data) {
+  if (error) {
+    return (
+      <VStack textColor="nord.nord0">
+        <Heading>No Data for Today</Heading>
+        <Text>{JSON.stringify(error, null, 4)}</Text>
+      </VStack>
+    );
+  }
+  if (!data) {
     return <Spinner size="xl" color="nord.nord0" thickness="0.25rem" />;
   }
-  if (data) {
-    const dataArray = data.data;
-    const parsedData: Array<DisplayProps> = dataArray[0].map(
-      ({ metric, value }: MetricData, i: number) => ({
-        metric: capitalize(metric),
-        todayVal: value,
-        variance: (value - dataArray[1][i].value) / dataArray[1][i].value,
-      })
-    );
-    return (
-      <Wrap w="100%" spacing="2rem">
-        {parsedData.map(({ metric, todayVal, variance }) => (
-          <WrapItem
-            key={metric}
-            flex={{ base: '1', md: '0 0 calc(50% - 2rem)' }}
-          >
-            <Metric metric={metric} todayVal={todayVal} variance={variance} />
-          </WrapItem>
-        ))}
-      </Wrap>
-    );
-  }
+  const dataArray = data.data;
+  const parsedData: Array<DisplayProps> = dataArray[0].map(
+    ({ metric, value }: MetricData, i: number) => ({
+      metric: capitalize(metric),
+      todayVal: value,
+      variance: (value - dataArray[1][i].value) / dataArray[1][i].value,
+    })
+  );
   return (
-    <VStack textColor="nord.nord0">
-      <Heading>No Data for Today</Heading>
-      <Text>{JSON.stringify(error, null, 4)}</Text>
-    </VStack>
+    <Wrap spacing="2rem">
+      <Flex
+        bgColor="nord.nord0"
+        flex="1 0 calc(100% - 2rem)"
+        p="1rem"
+        textColor="nord.nord6"
+        justify="flex-end"
+      >
+        <Text>{dayjs().toNow()}</Text>
+      </Flex>
+      {parsedData.map(({ metric, todayVal, variance }) => (
+        <WrapItem key={metric} flex={{ base: '1', md: '0 0 calc(50% - 2rem)' }}>
+          <Metric metric={metric} todayVal={todayVal} variance={variance} />
+        </WrapItem>
+      ))}
+    </Wrap>
   );
 };
 
